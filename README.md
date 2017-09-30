0b738d66edf5b7273d94af2a047aef142be82ee8
Radek <solaufein88@o2.pl>
2017-09-30 21:20:22 +0200

    fixes with bcrypt

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# travel
+# Project Travel
+
+##### Run app:
+- spring-boot:run
+
+##### Run app in debug mode:
+-  spring-boot:run "-Drun.jvmArguments=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000"
--- travelplanet/src/main/java/com/radek/travelplanet/config/WebSecurityConfig.java
+++ travelplanet/src/main/java/com/radek/travelplanet/config/WebSecurityConfig.java
@@ -1,16 +1,18 @@
 package com.radek.travelplanet.config;
 
-import com.radek.travelplanet.model.Employee;
 import com.radek.travelplanet.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.savedrequest.NullRequestCache;
 
 @Configuration
@@ -40,16 +42,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("admin").password("admin").roles("USER");
+//        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("USER");
 
-                auth
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(Employee.PASSWORD_ENCODER);
+        auth.authenticationProvider(authProvider());
     }
 
-//    @Bean
-//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(bCryptPasswordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
