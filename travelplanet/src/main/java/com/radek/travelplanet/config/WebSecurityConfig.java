d0de7e38f6b038296fbe428a1cd3b24a099f713e
Radek <radoslawwitek@gmail.com>
2018-04-09 21:31:12 +0200

    small cleanup2

--- travelplanet/src/main/java/com/radek/travelplanet/config/WebSecurityConfig.java
+++ travelplanet/src/main/java/com/radek/travelplanet/config/WebSecurityConfig.java
@@ -25,25 +25,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/travel/session").permitAll()
-                .antMatchers(HttpMethod.GET, "/travel/**").authenticated()
-                .antMatchers(HttpMethod.POST, "/travel/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT, "/travel/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/travel/**").hasRole("ADMIN")
-                .and()
-                .requestCache()
-                .requestCache(new NullRequestCache())
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER)
-                .and().csrf().disable();
+        http.authorizeRequests()
+            .antMatchers("/travel/session").permitAll()
+            .antMatchers(HttpMethod.GET, "/travel/**").authenticated()
+            .antMatchers(HttpMethod.POST, "/travel/**").hasRole("ADMIN")
+            .antMatchers(HttpMethod.PUT, "/travel/**").hasRole("ADMIN")
+            .antMatchers(HttpMethod.DELETE, "/travel/**").hasRole("ADMIN")
+            .and()
+            .requestCache()
+            .requestCache(new NullRequestCache())
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER)
+            .and().csrf().disable();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 //        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("USER");
-
         auth.authenticationProvider(authProvider());
     }
 
